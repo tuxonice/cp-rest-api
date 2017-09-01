95c6cd7760cefee8e962beae4c43482f37c32ba8
Helder Correia <tuxboy@tlab.pt>
2017-09-01 17:33:36 +0100

    Add district, municipality and location name to output

--- app/Http/Controllers/ApiV1/IndexController.php
+++ app/Http/Controllers/ApiV1/IndexController.php
@@ -10,23 +10,25 @@ use Illuminate\Support\Facades\DB;
 class IndexController extends Controller
 {
     
-    
     public function Index(Request $request, $cp3, $cp4)
     {
-        
-        
-        
-        $results = DB::table('ctt')->where([
-                    ['cp4', '=', $cp4],
-                    ['cp3', '=', $cp3],
-                        ])->get();
-        
-        
-        
+        $results = DB::table('ctt')
+            ->leftJoin('districts', 'ctt.district_id', '=', 'districts.district_id')
+            ->leftJoin('locations', 'ctt.location_id', '=', 'locations.location_id')
+            ->leftJoin('municipalities', function ($join) {
+                $join->on('ctt.municipality_id', '=', 'municipalities.municipality_id')->
+                on('municipalities.district_id', '=', 'ctt.district_id');
+                })
+            ->select('locations.name', 'districts.name AS district', 'municipalities.name AS municipality', 'ctt.*')
+            ->where([
+                ['ctt.cp4', '=', $cp4],
+                ['ctt.cp3', '=', $cp3],
+                 ])->get();
+          
+          
         $data = $results;
-        
         return response()->json($data);
-        
     }
     
 }
+
