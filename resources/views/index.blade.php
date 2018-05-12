667b6ca7c7ad8d46f1b3950e30ea7fb884c477d8
Helder Correia <tuxboy@tlab.pt>
2018-05-12 21:38:08 +0100

    Add frontpage view for how to use. Add License notice

---
+++ resources/views/index.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Zipcode API for Portugal</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <style>
+    body {
+        padding-top: 5rem;
+    }
+    
+    .starter-template {
+        padding: 3rem 1.5rem;
+        text-align: center;
+    }
+    </style>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <span class="navbar-brand">Zipcode API for Portugal</span>
+    </nav>
+    <div class="container">
+      <div class="row">
+          <div class="col-12-md">
+            <h2>1. Usage</h2>
+            <p><code>http://<?php echo($_SERVER['HTTP_HOST']); ?>/api/v1/{cp1}/{cp2}</code></p>
+            <p>Example: <a href="http://<?php echo($_SERVER['HTTP_HOST']); ?>/api/v1/1990/231">http://<?php echo($_SERVER['HTTP_HOST']); ?>/api/v1/1990/231</a></p>
+            <h2>2. Output</h2>
+            <p><pre>
+[
+  {
+    "name": "Lisboa",
+    "district": "Lisboa",
+    "municipality": "Lisboa",
+    "id": 136883,
+    "location_id": 21696,
+    "municipality_id": 6,
+    "district_id": 11,
+    "art_cod": "300460611",
+    "art_tipo": "Rossio",
+    "pri_prep": "dos",
+    "art_titulo": "",
+    "seg_prep": "",
+    "art_desig": "Olivais",
+    "art_local": "",
+    "troco": "",
+    "porta": "",
+    "cliente": "",
+    "cp4": "1990",
+    "cp3": "231",
+    "cpalf": "LISBOA",
+    "latitude": null,
+    "longitude": null
+  }
+]
+</pre></p>
+<h3>License</h3>
+<p>
+<small>THE SERVICE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.</small>
+</p>
+          </div>
+      </div>
+    </div>
+  </body>
+</html>
