f935aea804db7faf47a8a2cc0c861215a7aa52b7
Helder Correia <tuxboy@tlab.pt>
2018-06-02 11:44:16 +0100

    Normalize output

--- app/Http/Controllers/ApiV1/IndexController.php
+++ app/Http/Controllers/ApiV1/IndexController.php
@@ -25,7 +25,14 @@ class IndexController extends Controller
                 ['ctt.cp3', '=', $cp3],
                  ])->get();
 
-        return response()->json($results);
+        
+        
+        $data = [];
+        foreach($results as $result) {
+            $data[] = $this->normalizeData($result);
+        }
+
+        return response()->json($data);
     }
     
     
@@ -36,7 +43,7 @@ class IndexController extends Controller
                 ->inRandomOrder()
                 ->first();
 
-        $results = DB::table('ctt')
+        $result = DB::table('ctt')
             ->leftJoin('districts', 'ctt.district_id', '=', 'districts.district_id')
             ->leftJoin('locations', 'ctt.location_id', '=', 'locations.location_id')
             ->leftJoin('municipalities', function ($join) {
@@ -46,7 +53,51 @@ class IndexController extends Controller
             ->select('locations.name', 'districts.name AS district', 'municipalities.name AS municipality', 'ctt.*')
             ->where('ctt.id', $id->id)->first();
 
-        return response()->json($results);
+        return response()->json($this->normalizeData($result));
+    }
+    
+    protected function normalizeData($result)
+    {
+        $fullAddress = [];
+            if(!empty(trim($result->art_tipo))) {
+                $fullAddress[] = trim($result->art_tipo);
+            }
+            
+            if(!empty(trim($result->pri_prep))) {
+                $fullAddress[] = trim($result->pri_prep);
+            }
+            
+            if(!empty(trim($result->art_titulo))) {
+                $fullAddress[] = trim($result->art_titulo);
+            }
+            
+            if(!empty(trim($result->seg_prep))) {
+                $fullAddress[] = trim($result->seg_prep);
+            }
+            
+            if(!empty(trim($result->art_desig))) {
+                $fullAddress[] = trim($result->art_desig);
+            }
+            
+            if(!empty(trim($result->porta))) {
+                $fullAddress[] = trim($result->porta);
+            }
+            
+            return [
+                'locationName' => trim($result->name),
+                'fullAddress' => implode(' ', $fullAddress),
+                'zone' => trim($result->art_local),
+                'section' => trim($result->troco),
+                'doorNumber' => trim($result->porta),
+                'clientName' => trim($result->cliente),
+                'cp4' => trim($result->cp4),
+                'cp3' => trim($result->cp3),
+                'cpalf' => trim($result->cpalf),
+                'districtName' => trim($result->district),
+                'municipalityName' => trim($result->municipality),
+                'latitude' => $result->latitude,
+                'longitude' => $result->longitude,
+            ];
     }
     
 }
